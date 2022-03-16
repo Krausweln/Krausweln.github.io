86e9b6d9a62e768482ede22ff1f90e9fcb44e147
Krausweln <rizqimaulana442@gmail.com>
2022-03-16 13:43:58 +0700

    summintaskdfjlasfj

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-var screenWidth = window.screen.availWidth * window.devicePixelRatio;
-var screenHeight = window.screen.availHeight * window.devicePixelRatio;
+var screenWidth = window.screen.availWidth;
+var screenHeight = window.screen.availHeight;
 
 function responsive() {
   let navbar = document.querySelector(".navbar");
 
   if (screenWidth < 768) {
     navbar.classList.add("bg-light");
+  } else {
+    navbar.classList.remove("bg-light");
   }
 }
