f936dd67580cbc08f5b7beca62b93b001a419bb7
Krausweln <rizqimaulana442@gmail.com>
2022-03-24 17:36:22 +0700

    update on pictures format
    changed all the pictures in the index.html file to webp except for the wayang-bg

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ function bgChange() {
     i = 0;
   }
   if (i == 0) {
-    bgImage.style.backgroundImage = "url('img/borobudur.jpg')";
+    bgImage.style.backgroundImage = "url('img/borobudur.webp')";
     text.innerHTML = "Candi" + "<br>" + "Borobudur";
     bgImage.style.backgroundSize = "cover";
   } else if (i == 1) {
-    bgImage.style.backgroundImage = "url('img/garudawisnukencana.jpg')";
+    bgImage.style.backgroundImage = "url('img/garudawisnukencana.webp')";
     text.innerHTML = "Garuda Wisnu" + "<br>" + "Kencana";
     bgImage.style.backgroundSize = "cover";
   } else if (i == 2) {
-    bgImage.style.backgroundImage = "url('img/pantaikuta.jpg')";
+    bgImage.style.backgroundImage = "url('img/pantaikuta.webp')";
     bgImage.style.backgroundSize = "cover";
     text.innerHTML = "Pantai" + "<br>" + "Kuta";
   } else if (i == 3) {
-    bgImage.style.backgroundImage = "url('img/rajaampat.jpg')";
+    bgImage.style.backgroundImage = "url('img/rajaampat.webp')";
     text.innerHTML = "Raja" + "<br>" + "Ampat";
     bgImage.style.backgroundSize = "cover";
   }
